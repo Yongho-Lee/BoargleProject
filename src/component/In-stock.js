e5c09180000e332e3db7405f231d8f01e2165618
YONGHO LEE <ylee197@myseneca.com>
2022-06-06 19:13:58 -0400

    abbreviation modif

--- src/component/In-stock.js
+++ src/component/In-stock.js
@@ -5,10 +5,10 @@ function InStock(props) {
 
     let games = props.allGames;
 
-    let instkgames = [];
+    let instkGames = [];
     for(let i in games){                
         if(games[i].stock > 1){
-            instkgames.push(games[i])
+            instkGames.push(games[i])
         }
     }
 
@@ -19,7 +19,7 @@ function InStock(props) {
             <h4> In Stock Board Games </h4>           
 
                 {
-                    <Listing list={instkgames} />
+                    <Listing list={instkGames} />
                 }     
         </div>
 
