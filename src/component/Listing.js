1d3ac5c8606801e2716d9288275eb6a1935ed2aa
Yongho-Lee <ylee197@myseneca.ca>
2022-06-17 21:23:42 -0400

    css modf

--- src/component/Listing.js
+++ src/component/Listing.js
@@ -7,15 +7,16 @@ function Listing(props) {
     let listName = props.list;
     let navigate = useNavigate();
 
+    //<img className="product-image img-responsive center-block" onClick={() =>{navigate('../detail/'+listName[i].id)}} src={"https://raw.githubusercontent.com/Yongho-Lee/Boargle/main/src/img/bgImage/" + listName[i].shortName +".jpg?raw=true"} width="250px" height="200px" alt={'bgimage'+i} />
 
     return(
         <Container className="list-Container">
-            <Row xs={1} md={5} className="list-Container">
+            <Row xs={1} md={4} lg={5} className="list-Container">
         {
             listName.map(function(a,i){
                 return(
                     <Col sm key={i} className="product-info">
-                        <img className="product-image img-responsive center-block" onClick={() =>{navigate('../detail/'+listName[i].id)}} src={"https://raw.githubusercontent.com/Yongho-Lee/Boargle/main/src/img/bgImage/" + listName[i].shortName +".jpg?raw=true"} width="250px" height="200px" alt={'bgimage'+i} />
+                        <img className="product-image" onClick={() =>{navigate('../detail/'+listName[i].id)}} src={"https://raw.githubusercontent.com/Yongho-Lee/Boargle/main/src/img/bgImage/" + listName[i].shortName +".jpg?raw=true"} width="250px" height="200px" alt={'bgimage'+i} />
                         <p className="product-name"> {listName[i].name} </p>
                         <p className="product-price"> ${listName[i].price} CAD </p>
                         {stockCheck(listName[i].stock)}
