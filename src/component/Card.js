aedf8643a9987442a02a3def2c59d65cf6dfd68f
Yongho-Lee <ylee197@myseneca.ca>
2022-07-04 22:15:26 -0400

    Card-image

--- src/component/Card.js
+++ src/component/Card.js
@@ -18,7 +18,7 @@ function Card(props){
 
             return(
                 <Col sm key={i}>           
-                    <img onClick={() =>{navigate('./products/detail/'+bcd)}} src={require("../img/bgImage/"+ name +".jpg?raw=true")} width="250px" height="200px" alt={'bgimage'+i} />
+                    <img onClick={() =>{navigate('./products/detail/'+bcd)}} src={require("../img/bgImage/"+ name +".jpg?raw=true")} width="250px" height="200px" alt={'bgimage'+i} className="product-image"/>
                     <h4> {lists[0].data[i].name} </h4>
                     <p> {lists[0].data[i].price} </p>
                 </Col>                     
@@ -29,14 +29,22 @@ function Card(props){
           </Row>
           <Row>
             <p> Best Selling Family Games </p>
+
           </Row>
           
           <Row>
-
           {/* {
             <BestFamily lists={lists}/>
   
           } */}
+
+          <Col sm key={1}>           
+                    <img onClick={() =>{navigate('./products/detail/29877030712')}} src={require("../img/bgImage/catan.jpg?raw=true")} width="250px" height="200px" alt={'bestGame1'} className="product-image" />
+                    <h4> {lists[0].data[5].name} </h4>
+                    <p> {lists[0].data[5].price} </p>
+          </Col>     
+          
+
             </Row>
         </Container>
      
@@ -60,5 +68,7 @@ function Card(props){
 //         console.log(props.boardGameList[1].data)
    
 //   }
+
+
 
   export default Card;
