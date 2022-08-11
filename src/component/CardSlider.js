51e04db284e5b6d78bb1e24bd376d55b435248e4
Yongho-Lee <ylee197@myseneca.ca>
2022-08-10 22:10:11 -0400

    slider dividing

---
+++ src/component/CardSlider.js
@@ -0,0 +1,16 @@
+import { Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
+
+import stockCheck from '../js/stockCheck';
+
+import "slick-carousel/slick/slick.css";
+import "../slick-theme.css";
+import Slider from "react-slick";
+
+
+function CardSlider(props) {
+
+}
+
+export default CardSlider;
